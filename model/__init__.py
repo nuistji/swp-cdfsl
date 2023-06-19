29dfc505eab0dd3753433e6b19fde16d814f6d63
nuistji <31852247+nuistji@users.noreply.github.com>
2023-06-19 16:04:35 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,22 @@
+from model.base import BaseModel
+from model.byol import BYOL
+from model.moco import MoCo
+from model.simclr import SimCLR
+from model.simsiam import SimSiam
+from model.swav import SwAV
+_model_class_map = {
+    'base': BaseModel,
+    'simclr': SimCLR,
+    'byol': BYOL,
+    'moco': MoCo,
+    'simsiam': SimSiam,
+    'swav': SwAV
+}
+
+
+def get_model_class(key):
+    if key in _model_class_map:
+        return _model_class_map[key]
+    else:
+        raise ValueError('Invalid model: {}'.format(key))
 
